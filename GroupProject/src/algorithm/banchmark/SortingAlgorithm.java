a3e0c16b83d5ca5f08067756057324b72c4f5a98
Vikram Ahir <ahir345@gmail.com>
2016-09-23 08:50:26 +0530

    addition
    unic program.

--- GroupProject/src/algorithm/banchmark/SortingAlgorithm.java
+++ GroupProject/src/algorithm/banchmark/SortingAlgorithm.java
@@ -1,16 +1,19 @@
 package algorithm.banchmark;
 
+import java.util.Scanner;
+
 public class SortingAlgorithm {
 	
 	public static void main(String[] args){
 		System.out.println("hello fnds..");
 		
-		int a = 10;
-		int b = 20;
-		int c;
+		Scanner s= new Scanner(System.in);
+		System.out.println("Enter first value :");
+		int num1=s.nextInt();
+		System.out.println("Enter second value :");
+		int num2=s.nextInt();
+		System.out.println("Addition of two value is :"+(num1+num2));
 		
-		c = a + b;
-		System.out.println(c);
 	}
 	
 
